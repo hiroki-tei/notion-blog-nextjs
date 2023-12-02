017c70884b156bfbe20a326a9d65ae95bbf9f1b1
hiroki-tei <hirokiki18@gmail.com>
2023-12-02 18:46:49 +0900

    render acustom rticle contents in builder.io made blog page

--- app/builder/blog/[slug]/page.jsx
+++ app/builder/blog/[slug]/page.jsx
@@ -1,10 +1,24 @@
 import { builder } from "@builder.io/sdk";
 import { RenderBuilderContent } from "../../../../components/builder";
 
+import {
+  getDatabase, getBlocks, getPageFromSlug,
+} from '../../../../lib/notion';
+
 // Builder Public API Key set in .env file
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY);
 
+export async function generateStaticParams() {
+  const database = await getDatabase();
+  return database?.map((page) => {
+    const slug = page.properties.Slug?.formula?.string;
+    return { id: page.id, slug };
+  });
+}
+
 export default async function Page(props) {
+  const page = await getPageFromSlug(props.params?.slug);
+  const blocks = await getBlocks(page?.id);
   const content = await builder
     // Get the page content from Builder with the specified options
     .get("page", {
@@ -16,10 +30,18 @@ export default async function Page(props) {
     // Convert the result to a promise
     .toPromise();
 
+  const data = {
+    page,
+    blocks,
+    article: {
+      title: page.properties.Page.title[0].text.content
+    }
+  }
+
   return (
     <>
       {/* Render the Builder page */}
-      <RenderBuilderContent content={content} />
+      <RenderBuilderContent content={content} data={data} />
     </>
   );
 }
--- app/builder/blog/page.jsx
+++ app/builder/blog/page.jsx
@@ -1,10 +1,15 @@
 import { builder } from "@builder.io/sdk";
 import { RenderBuilderContent } from "../../../components/builder";
+import {
+  getBlocks, getPageFromSlug,
+} from '../../../lib/notion';
 
 // Builder Public API Key set in .env file
 builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY);
 
 export default async function Page(props) {
+  const page = await getPageFromSlug("qgis-python-add-layer-into-only-specified-group")
+  const blocks = await getBlocks(page?.id);
   const content = await builder
     // Get the page content from Builder with the specified options
     .get("page", {
@@ -16,10 +21,17 @@ export default async function Page(props) {
     // Convert the result to a promise
     .toPromise();
 
+  const data = {
+    page,
+    blocks,
+    article: {
+    }
+  }
+
   return (
     <>
       {/* Render the Builder page */}
-      <RenderBuilderContent content={content} />
+      <RenderBuilderContent content={content} data={data} />
     </>
   );
 }
