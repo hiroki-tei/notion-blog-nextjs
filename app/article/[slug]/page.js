a27cb88dec04f5bfe83314515be1c2337d6cc23a
hiroki-tei <hirokiki18@gmail.com>
2023-12-02 18:39:56 +0900

    separate Article HTML into another component

--- app/article/[slug]/page.js
+++ app/article/[slug]/page.js
@@ -1,13 +1,7 @@
-import { Fragment } from 'react';
-import Head from 'next/head';
-import Link from 'next/link';
-
 import {
-  getDatabase, getBlocks, getPageFromSlug,
+  getDatabase, getBlocks, getPageFromSlug
 } from '../../../lib/notion';
-import Text from '../../../components/text';
-import { renderBlock } from '../../../components/notion/renderer';
-import styles from '../../../styles/post.module.css';
+import { Article } from '../../../components/notion/article';
 
 // Return a list of `params` to populate the [slug] dynamic segment
 export async function generateStaticParams() {
@@ -18,36 +12,15 @@ export async function generateStaticParams() {
   });
 }
 
-export default async function Page({ params }) {
-  const page = await getPageFromSlug(params?.slug);
+export default async function Page(props) {
+  const page = await getPageFromSlug(props.params?.slug);
   const blocks = await getBlocks(page?.id);
 
   if (!page || !blocks) {
     return <div />;
   }
 
-  return (
-    <div>
-      <Head>
-        <title>{page.properties.Title?.title[0].plain_text}</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-
-      <article className={styles.container}>
-        <h1 className={styles.name}>
-          <Text title={page.properties.Title?.title} />
-        </h1>
-        <section>
-          {blocks.map((block) => (
-            <Fragment key={block.id}>{renderBlock(block)}</Fragment>
-          ))}
-          <Link href="/" className={styles.back}>
-            ← Go home
-          </Link>
-        </section>
-      </article>
-    </div>
-  );
+  return <Article page={page} blocks={blocks} />
 }
 
 // export const getStaticPaths = async () => {
