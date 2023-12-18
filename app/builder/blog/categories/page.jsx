e58572745aa1a71a025f927a69b3f58577e8b283
hiroki-tei <hirokiki18@gmail.com>
2023-12-18 17:46:23 +0900

    [feat] create categories page which shows its belonging labels

---
+++ app/builder/blog/categories/page.jsx
@@ -0,0 +1,47 @@
+import { builder } from "@builder.io/sdk";
+import { RenderBuilderContent } from "../../../../components/builder";
+
+import {
+  listLabelsFromCategory
+} from '../../../../lib/notion';
+
+// Builder Public API Key set in .env file
+builder.init(process.env.NEXT_PUBLIC_BUILDER_API_KEY);
+
+export default async function Page(props) {
+  /* eslint-disable implicit-arrow-linebreak, comma-dangle, function-paren-newline */
+  const testCategory = 'Resume'
+  const labels = await listLabelsFromCategory(testCategory);
+  const data = {
+    //page:
+    //  pages
+    //    .filter(page => page?.properties?.Slug?.rich_text?.length > 0)
+    //    .map(page=> {
+    //      const slug = page.properties.Slug.rich_text[0].plain_text
+    //        return {
+    //          slug,
+    //          title: page.properties.Page.title[0].plain_text,
+    //          url: `/builder/blog/${slug}`
+    //        }
+    //    }),
+    category: testCategory,
+    labels
+  }
+  const content = await builder
+    // Get the page content from Builder with the specified options
+    .get("page", {
+      userAttributes: {
+        // Use the page path specified in the URL to fetch the content
+        urlPath: "/" + "builder/" + "blog/" + "categories/" +(props?.params?.page?.join("/") || ""),
+      },
+    })
+    // Convert the result to a promise
+    .toPromise();
+
+  return (
+    <>
+      {/* Render the Builder page */}
+      <RenderBuilderContent content={content} data={data} />
+    </>
+  );
+}
