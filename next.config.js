928354dc8bd9ca46589a8dfdaaf11b278f5caa8a
hiroki-tei <hirokiki18@gmail.com>
2023-12-27 23:24:25 +0900

    [fix] fail in requesting server actions 500

--- next.config.js
+++ next.config.js
@@ -1,6 +1,10 @@
 const withBuilderDevTools = require("@builder.io/dev-tools/next")();
 
 /** @type {import('next').NextConfig} */
-const nextConfig = withBuilderDevTools({});
+const nextConfig = withBuilderDevTools({
+  experimental: {
+    serverMinification: false
+  }
+});
 
 module.exports = nextConfig;
