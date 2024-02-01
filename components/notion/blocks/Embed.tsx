4056dd53f387876701234916d8dedc3aee25d336
hiroki-tei <hirokiki18@gmail.com>
2024-02-01 16:21:59 +0900

    fetch gist page data on newly created Embed component

---
+++ components/notion/blocks/Embed.tsx
@@ -0,0 +1,24 @@
+import React, { useState, useEffect } from 'react';
+import { fetchUrl } from '@actions/fetch'
+
+export const Embed = ({ url }) => {
+  const id = url.match(/(\.com\/)(.*?)([^#]+)/)?.pop() ?? null
+  const file = url.split('#').pop()
+  const fileQS = file.match(/file*/)
+    ? `&file=${file.replace('file-', '').replace('-', '.')}`
+    : ''
+  const urlTarget = `https://gist.github.com/${id}.json${fileQS}`
+  const [gistContent, setGistContent] = useState<any>()
+  const fetchGist = fetchUrl.bind(null, urlTarget)
+  useEffect(() => {
+    fetchGist().then( data => {
+      console.log(data)
+      setGistContent(data)
+    })
+  }, [])
+
+  return (
+    <div>{gistContent.div}</div>
+  )
+
+}
