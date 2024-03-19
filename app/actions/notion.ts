35a8d5c165344608c6f4a6d5d4ec4e589f717dc6
Tei Hiroki <teihiroki@Teis-MacBook-Pro.local>
2024-03-19 14:51:10 +0900

    Revert "Merge pull request #43 from hiroki-tei/rerender_when_expired"
    This reverts commit 8903b16a691072c4c55f1c8685d3d973220de54b, reversing
    changes made to 649f9fe051963b4a17654b5c867acbd77ad26269.

--- app/actions/notion.ts
+++ app/actions/notion.ts
@@ -1,12 +1,7 @@
 'use server'
-import { getPage, getBlocks } from "@lib/notion"
+import { getPage } from "@lib/notion"
 
 export async function getPageAction(pageID: string) {
   const data = await getPage(pageID)
-  return data
-}
-
-export async function getBlockAction(blockID: string) {
-  const data = await getBlocks(blockID)
   return data
 }
